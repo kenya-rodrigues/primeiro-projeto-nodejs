d313dc76a32f9f729e610255815a464d758248c2
kenya-rodrigues <kenyarodrigues@usp.br>
2022-04-12 14:51:10 -0300

    Added get statement and post account routes

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.get("/statement/:documentNumber", (request, response) => {
+  const { documentNumber } = request.params;
+
+  const customer = customers.find((customer) => customer.documentNumber === documentNumber);
+
+  return response.json(customer.statement);
+});
+
+app.post("/accounts", (request, response) => {
+  const { documentNumber, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.documentNumber === documentNumber
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists" });
+  }
+
+  customers.push({
+    documentNumber,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.listen(3332);
